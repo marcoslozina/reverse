c995549e166d4fd9319f7e3cbf41aea7458f159a
Marcos Lozina <marcoslozina@gmail.com>
2022-03-05 23:47:41 -0300

    subida inicial

---
+++ ms-customer-backend/src/main/java/academy/digitallab/store/customer/autoconfigure/PersistenceConfig.java
@@ -0,0 +1,11 @@
+package academy.digitallab.store.customer.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing
+public class PersistenceConfig {
+
+}
